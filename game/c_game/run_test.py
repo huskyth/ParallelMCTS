9b032b74251a25f1cf3cbdc02cb3201a97213d38
huskyth <1622313177@qq.com>
2026-08-15 11:10:42 +0800

    f

--- game/c_game/run_test.py
+++ game/c_game/run_test.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0, './build')   # 将 build 目录加入 Python 路径
 
 # 从 wmchess 包导入你的训练模块
-from wmchess.eva_test import main()
+from wmchess.eva_test import main
 
 if __name__ == "__main__":
     main()
